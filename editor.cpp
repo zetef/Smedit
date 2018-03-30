2abb29f70247d0a823d5d2c66bd85a85df4213f9
zetef <dediustef@gmail.com>
2018-03-31 01:48:50 +0300

    the code for the editor

--- editor.cpp
+++ editor.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-//#include <conio.h>
 #include <dirent.h>
 
 using namespace std;
@@ -33,16 +32,25 @@ int edit(){
 
 int main()
 {
-    // use "." for current folder. use ".." for previous folder
+    // use "." for current folder. use ".." for previous folder. starting pos need no change
     dir = opendir(".");
+
+    cout << "where to store the files from the editor" << endl << endl;
 
     //lists elements from dir
     while(pdir = readdir(dir)){
-        cout << i << " " << pdir -> d_name << endl;
+        if(i == 1)
+            cout << pdir -> d_name << " (current directory)" << endl;
+        if(i == 2)
+            cout << pdir -> d_name << " (previous directory)" << endl;
+        else if(i != 1)
+            cout << pdir -> d_name << endl;
         i++;
     }
     closedir(dir);
 
+    cout << endl << "chose directory" << endl;
+        
     //get folder names with spaces
     getline(cin, name_dir);
 
