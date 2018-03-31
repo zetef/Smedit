60eb40f04c8ff0038994692b9f9f5ac1ecce991b
zetef <dediustef@gmail.com>
2018-04-01 01:28:17 +0300

    the code

--- editor.cpp
+++ editor.cpp
@@ -2,9 +2,16 @@
 #include <fstream>
 #include <string>
 #include <dirent.h>
+#include <windows.h>
+#include <time.h>
 
 using namespace std;
 
+const WORD colors[] =
+		{
+		0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F,
+		0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6
+		};
 int i(1);
 DIR *dir;
 dirent *pdir;
@@ -17,25 +24,43 @@ int edit(){
     string path = name_dir + "\\" + name;
     ofstream out(path);
     while(getline(editor, n)){
+        //new file command
         if(n == "$stop")
             edit();
+        //stop editing command
         if(n == "$stopf")
             return 0;
+        //read more characters
         while(n.length() == 0)
             getline(editor, n);
+        //comment
+        if(n[0] == '#' ||
+           //in case you comment after an extension declaration
+           (n[0] == ' ' && n[1] == '#'))
+            continue;
+        //write the string
         if(!editor.eof())
             out << n << " ";
         out << endl;
     }
     editor.close();
 }
-
 int main()
 {
-    // use "." for current folder. use ".." for previous folder. starting pos need no change
-    dir = opendir(".");
+    //some color action
+    srand(time(NULL));
+	HANDLE hstdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	//remember the original state
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo( hstdout, &csbi );
+	//setting the actual color
+	auto randColor = rand() % 10 + 2; //i did 10 + 2 so I don't get the first case where the text and bg in black
+    SetConsoleTextAttribute(hstdout, randColor);
 
     cout << "where to store the files from the editor" << endl << endl;
+
+    // use "." for current folder. use ".." for previous folder. starting pos need no change
+    dir = opendir(".");
 
     //lists elements from dir
     while(pdir = readdir(dir)){
@@ -50,7 +75,10 @@ int main()
     closedir(dir);
 
     cout << endl << "chose directory" << endl;
-        
+
+    //restore normal color
+    SetConsoleTextAttribute(hstdout, csbi.wAttributes);
+
     //get folder names with spaces
     getline(cin, name_dir);
 
@@ -58,4 +86,3 @@ int main()
     mkdir(name_dir.c_str());
     edit();
 }
-
